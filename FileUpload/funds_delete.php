ed35c1a3489d412f1a73eb10ba825c6a834b8100
formula5041 <formula5041@gmail.com>
2021-08-13 01:33:47 +0800

    '完成Active頁面透過會員編輯的資料渲染'

--- FileUpload/funds_delete.php
+++ FileUpload/funds_delete.php
@@ -4,13 +4,14 @@
     // DONATE
     $donate = htmlspecialchars($_POST["donate"]);
 
-    $sql= "DELETE from DONATE where donate_id = ?";
-    $statement = $pdo ->prepare($sql);
-    $statement -> bindValue(1, $donate);
-    $statement -> execute();
-
     $sql= "DELETE from DONATEOPTION where donate = ?";
     $statement = $pdo ->prepare($sql);
     $statement -> bindValue(1, $donate);
     $statement -> execute();
+
+    $sql= "DELETE from DONATE where donate_id = ?";
+    $statement = $pdo ->prepare($sql);
+    $statement -> bindValue(1, $donate);
+    $statement -> execute();
+  
 ?>

317afba5de372d22844246e9636a1874f0b70ec7
formula5041 <formula5041@gmail.com>
2021-08-19 17:41:27 +0800

    '完成演出活動分享'

--- carousel_component_get.php
+++ carousel_component_get.php
@@ -5,7 +5,7 @@
 
     $sql = "SELECT e.*, d.account FROM ACTIVITY e
             join MEMBER d
-            on d.member_id = e.initiator Limit 5";
+            on d.member_id = e.initiator order by e.setup_date desc limit 5";
 
     $statement = $pdo->query($sql);
     $data = $statement->fetchAll();
--- funds_page_detailMain_get.php
+++ funds_page_detailMain_get.php
@@ -3,7 +3,12 @@
 
     $donate_id = $_POST["donate_id"];
     
-    $sql = "SELECT *, TIMESTAMPDIFF(DAY, NOW(), end_date) AS countdownDate from DONATE where donate_id = $donate_id";
+    // $sql = "SELECT *, TIMESTAMPDIFF(DAY, NOW(), end_date) AS countdownDate from DONATE where donate_id = $donate_id";
+    $sql ="SELECT d.*, TIMESTAMPDIFF(DAY, NOW(), d.end_date) AS countdownDate, m.account
+	from DONATE d
+    join member m
+    on d.initiator = m.member_id
+    where donate_id = $donate_id";
 
     $statement = $pdo->query($sql);
     $data = $statement->fetchAll();
